687419a45dccb032c0eadb24638849e3d5291cc8
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-10-03 21:09:36 -0400

    Completed get /api/posts/:id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -5,13 +5,35 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   Posts.find()
-    .then((posts) => {
-      res.status(201).json(posts);
-    })
+    .then((posts) => res.status(201).json(posts))
     .catch((err) => {
       res
         .status(500)
         .json({ message: 'The posts information could not be retrieved' });
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    res.status(404).json({ message: 'Please enter an id' });
+  }
+
+  Posts.findById(id)
+    .then((post) => {
+      if (!post) {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exust' });
+      }
+
+      res.status(201).json(post);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: 'The post information could not be retrieved' });
     });
 });
 
