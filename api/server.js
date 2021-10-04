adc010a621ae9deb4afba7ef523454a5c8da8f01
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-10-03 20:47:39 -0400

    initial server files for basic host of server

--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
-// implement your server here
-// require your posts router and connect it here
+// imports
+const express = require('express');
+
+// initialization
+const server = express();
+
+// middleware
+server.use(express.json());
+
+// endpoints
+server.get('/', (req, res) => {
+  res.status(201).json({ message: 'server do be running fam' });
+});
+
+module.exports = server;
