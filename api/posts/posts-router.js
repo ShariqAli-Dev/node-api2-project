51041512c0eb1fee4f1525ef29c0898751ff7cc6
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-10-03 20:58:22 -0400

    completed get api/posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const Posts = require('./posts-model');
 
 const router = express.Router();
 
-router.get('/applepies', (req, res) => {
-  res.status(201).json({ message: 'APPLE A DAY KEEPS THE DOCTOR AWAY' });
+router.get('/', (req, res) => {
+  Posts.find()
+    .then((posts) => {
+      res.status(201).json(posts);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: 'The posts information could not be retrieved' });
+    });
 });
 
 module.exports = router;
